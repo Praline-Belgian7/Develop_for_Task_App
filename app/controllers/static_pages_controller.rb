9a490c9b40169ad2f968a2adef319d31c1452940
EC2 Default User <ec2-user@ip-172-31-45-44.ap-northeast-1.compute.internal>
2020-04-20 09:04:48 +0000

    B1~8 Finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,24 @@
 class StaticPagesController < ApplicationController
+  
   def top
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  
 end
