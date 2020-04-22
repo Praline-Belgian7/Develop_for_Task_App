6cc88bb1b88c2f60f18eab4a9aad0e3f2fd30445
EC2 Default User <ec2-user@ip-172-31-45-44.ap-northeast-1.compute.internal>
2020-04-22 22:58:33 +0000

    C7 Finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,22 @@ class TasksController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:user_id])
+    @task = Task.find_by(id: @user.id)
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @task = Task.find_by(id: @user.id)
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_tasks_path @user
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def task_params
